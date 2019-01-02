7752bb60c112bccd500c5b90c67df413221bf67c
Chan Kin Long <24862555+cklll@users.noreply.github.com>
2019-01-03 00:58:57 +0800

    initial commit

---
+++ web-spa-nextjs/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb",
+    "env": {
+        "node": true,
+    },
+    "rules": {
+        "indent": ["error", 4],
+        "react/jsx-indent": ["error", 4],
+        "react/jsx-filename-extension": 0,
+    },
+};
