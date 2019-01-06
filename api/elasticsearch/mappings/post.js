20f0c4912633728794b72912ea4cf759a4bbfb2d
Chan Kin Long <24862555+cklll@users.noreply.github.com>
2019-01-06 18:38:28 +0800

    import data

---
+++ api/elasticsearch/mappings/post.js
@@ -0,0 +1,30 @@
+const mappings = {
+    _doc: {
+        dynamic: 'strict',
+        properties: {
+            id: { type: 'long' },
+            short_code: { type: 'text' },
+            like_count: { type: 'long' },
+            comment_count: { type: 'long' },
+            caption: { type: 'text' },
+            taken_at: {
+                type: 'date',
+                format: 'epoch_second',
+            },
+            media_urls: {
+                type: 'text',
+                    fields: {
+                    keyword: {
+                        type: 'keyword',
+                        ignore_above: 256
+                    }
+                }
+             },
+            username: { type: 'text' },
+        }
+    }
+};
+
+module.export = {
+    mappings,
+};
--- api/elasticsearch/seed/import.js
+++ api/elasticsearch/seed/import.js
@@ -1,4 +1,6 @@
 const esClient = require('../client');
+const postMappings = require('../mappings/post');
+const postData = require('../seed/data.json');
 
 (async () => {
     console.log('deleting post index');
@@ -18,61 +20,30 @@ const esClient = require('../client');
     await esClient.indices.create({
         index: 'post',
         body: {
-            mappings : {
-                _doc : {
-                    properties : {
-                        id : { type : 'long' },
-                        short_code : { type : 'text' },
-                        like_count : { type : 'long' },
-                        comment_count : { type : 'long' },
-                        hash_tags : { type : 'long' },
-                        caption : { type : 'text' },
-                        taken_at : {
-                            type: 'date',
-                            format: 'epoch_second',
-                        },
-                        media_urls : { type : 'long' },
-                        username : { type : 'text' },
-                    },
-                },
-            },
+            mappings: postMappings,
         },
     });
     console.log('success', '\n');
 
-    // console.log('importing data');
-    // await esClient.bulk({
-    //     index: 'post',
+    console.log(`importing ${postData.length} post data`);
+    const bulkBody = postData.reduce((acc, item) => {
+        acc.push({ index:  { _index: 'post', _type: '_doc', _id: Number(item.id) } },)
+        acc.push({
+            ...item,
+            id: Number(item.id),
+        });
+        return acc;
+     }, []);
+    const resp = await esClient.bulk({
+        body: bulkBody,
+    });
 
-    //     body: [
-    //       // action description
-    //       { index:  { _index: 'myindex', _type: 'mytype', _id: 1 } },
-    //        // the document to index
-    //       { title: 'foo' },
-    //       // action description
-    //       { update: { _index: 'myindex', _type: 'mytype', _id: 2 } },
-    //       // the document to update
-    //       { doc: { title: 'foo' } },
-    //       // action description
-    //       { delete: { _index: 'myindex', _type: 'mytype', _id: 3 } },
-    //       // no document needed for this delete
-    //     ]
-    //   });
-
-    // "like_count": 64984,
-    // "comment_count": 64984,
-    // "caption": "The king on a walkabout.\n-\n\ud83c\udfa5 @primuscat\n-\n@meowed #cat #majestictail #9gag",
-    // "id": "1948017988724710605",
-    // "short_code": "BsIwAoqAdzN",
-    // "hash_tags": [
-    //     "majestictail",
-    //     "cat",
-    //     "9gag"
-    // ],
-    // "taken_at": 1546441881,
-    // "media_urls": [
-    //     "https://instagram.fhkg1-1.fna.fbcdn.net/vp/a3d5c71dafd314fcd1538eca9181d88c/5C2FCFBF/t50.2886-16/49787800_945798905624870_7435423322428407808_n.mp4?_nc_ht=instagram.fhkg1-1.fna.fbcdn.net"
-    // ],
-    // "username": "9gag"
-    // console.log('success', '\n');
+    if (resp.errors) {
+        resp.items.filter(Boolean(item.index.error)).forEach((item) => {
+            console.log(item.index.error);
+        });
+        throw new Error('UNKNOWN ERROR');
+    }
+    
+    console.log(`success; took ${resp.took}`, '\n');
 })();
